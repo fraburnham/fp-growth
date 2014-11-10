59e492870da223b9cd93df25f3bbd4ebb93f0f84
Frank Burnham <seditiosus@debian>
2014-11-10 13:25:45 -0500

    better form for build-path

--- src/fp_growth/core.clj
+++ src/fp_growth/core.clj
@@ -4,12 +4,8 @@
 ;;implement the fp growth algorithm in clojure
 ;;to be used for basket analysis
 
-;replace all instances of tree with loc for
-;consistency with clojure.zip
-
 (defmacro terminal-node? [loc]
   `(nil? (zip/down ~loc)))
-;if a node is non-terminal a double zip is needed to get to the "node"?
 
 (defmacro empty-tree []
   `(zip/seq-zip (seq '())))
@@ -18,23 +14,21 @@
   (if (terminal-node? loc) nil
       (zip/down loc)))
 
-(defn print-tree [tree]
-  (if (zip/end? tree) 
+(defn print-tree [loc]
+  (if (zip/end? loc 
     nil
     (do
-      (println (zip/node tree))
-      (recur (zip/next tree)))))
+      (println (zip/node loc))
+      (recur (zip/next loc)))))
 
-(defn print-terminal-nodes [tree]
-  (if (zip/end? tree)
+(defn print-terminal-nodes [loc]
+  (if (zip/end? loc)
     nil
-    (if (terminal-node? tree)
-      (do ;if zip/down is nil this is a termial node
-        (println (zip/node tree))
-        (recur (zip/next tree)))
-      (recur (zip/next tree)))))
-;should be able to use terminal-node checking to
-;decide if we can add a child
+    (if (terminal-node? loc)
+      (do
+        (println (zip/node loc))
+        (recur (zip/next loc)))
+      (recur (zip/next loc)))))
 
 (defn alpha-by-first-char [x]
   (sort-by (comp str first) compare x))
@@ -52,42 +46,54 @@
                         (alpha-by-first-char (map item-freq-list %)))
               data))))
 
-(defn tree-find [tree find-node]
-  (cond (= (zip/node tree) find-node) tree
-        (zip/end? tree) nil
-        :else (recur (zip/next tree) find-node)))
-
-;some edit magic maybe. dunno it worked on one node
-;(zip/edit search-tree 
-;          (fn [node]
-;            (let [val (inc ((first path) (zip/node search-tree)))]
-;              {(first path) val})))
-
-;now the path
-;I just noticed it looks like the tree is being made wrong
-;even on the first pass with only one path to make
-;(nil
-; ({b 1} 
-;  ({a 1})) 
-; ({c 1}))
-;should be
-;(nil
-; ({b 1}
-;  ({a 1}
-;   ({c 1}))))
-(defn build-path [tree path]
-  (if (empty? path) 
-    (zip/seq-zip (zip/root tree))
-    (recur
-     (zip/next (zip/insert-right tree (list {(first path) 1})))
-     (rest path))))
-
+(defn tree-find [loc find-node]
+  (cond (= (zip/node loc) find-node) loc
+        (zip/end? loc) nil
+        :else (recur (zip/next loc) find-node)))
 
 (defn next-node [loc]
-  (if (terminal-node? loc) 
-    (doublezip loc)
-    (zip/next loc)))
+  (let [nloc (zip/next loc)]
+    (if (zip/branch? nloc)
+      (recur nloc)
+      nloc)))
 
-(zip/append-child (-> fp-tree zip/next doublezip doublezip zip/up) {'z 1})
-;first zip/next is because fp-tree was poorly formatted
-;doublezip gets us to the node instead of the branch
+(defn next-branch [loc]
+  (let [nloc (zip/next loc)]
+    (if (zip/branch? nloc) 
+      nloc
+      (recur nloc))))
+
+(defn add-branch [loc]
+  (zip/edit loc (fn [[node _]] (conj '(()) node))))
+
+;inc the val in the dict that the node holds
+(defn update-node [loc]
+  (zip/edit 
+   loc 
+   (fn [node]
+     (let [k (first (keys node))]
+       {k (inc (k node))}))))
+
+;takes an (empty-tree) as loc
+;or an exsisting tree to add to
+(defn build-path [loc path]
+  (let [new-node {(first path) 1}]
+    (if (empty? path) 
+      (zip/seq-zip (zip/root loc))
+      ;sweet, so the concept is working, but the search is weak
+      ;it'll need to look IN the dict to see if the keys match
+      ;work on that there and then the first two thirds of fp-growth
+      ;are ready
+      (let [next-node (tree-find loc new-node)]
+        (println (zip/node next-node) new-node)
+        (if (= (zip/node next-node) new-node)
+          (recur
+           (update-node next-node)
+           (rest path))
+          (recur
+            (zip/append-child (next-branch (add-branch loc)) new-node)
+            (rest path)))))))
+
+;ok, I'm getting it branch->node->branch->node
+;nodes hold our data
+;and branches describe where the nodes are
