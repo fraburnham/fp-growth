e136f8671788563cede5144715116fc4dfb25901
Frank Burnham <seditiosus@debian>
2014-11-11 18:07:17 -0500

    cleanup unused functions

--- src/fp_growth/core.clj
+++ src/fp_growth/core.clj
@@ -1,28 +1,14 @@
 (ns fp-growth.core
   :require [clojure.zip :as zip])
 
-(defmacro terminal-node? [loc]
-  `(nil? (zip/down ~loc)))
-
 (defmacro empty-tree []
   `(zip/seq-zip (seq '())))
 
-(defn first-child [loc]
-  (if (terminal-node? loc) nil
-      (zip/down loc)))
-
-;requires that a branch is passed
 (defn next-subbranch [loc]
   (zip/right (zip/down loc)))
 
 (defn alpha-by-first-char [x]
   (sort-by (comp str first) compare x))
-
-(defn next-node [loc]
-  (let [nloc (zip/next loc)]
-    (if (zip/branch? nloc)
-      (recur nloc)
-      nloc)))
 
 (defn next-branch [loc]
   (let [nloc (zip/next loc)]
@@ -39,8 +25,6 @@
 
 ;data is in the format
 ;((item item item) (item item item))
-;a transaction is a single list of items
-;I'm using closures to increase readability, is that bad practice?
 (defn pre-sort [data]
   (let [freqs (apply (partial merge-with +) (map frequencies data))]
     (defn item-freq-list [x]
@@ -49,28 +33,6 @@
          (map #(sort-by last > 
                         (alpha-by-first-char (map item-freq-list %)))
               data))))
-
-(defn tree-find [loc find-node]
-  (cond (= (zip/node loc) find-node) loc
-        (zip/end? loc) nil
-        :else (recur (zip/next loc) find-node)))
-
-(defn tree-find-key-in-map-subbranch [loc find-key]
-  ;the if below may be superflous
-  (if (not (nil? (zip/right loc)))
-    (tree-find-key-in-map-subbranch ((zip/right loc) find-key)))
-  (if (nil? loc) ;hit the bottom of this branch, don't look into the next one
-    nil
-    (let [node (zip/node (zip/down loc))
-          k (if (= (type node) (type {}))
-              (first (keys node))
-              nil)]
-      (cond (= k find-key) loc ;return the loc where the key is
-            (zip/end? loc) nil ;this may never happen ...
-            :else (recur (next-subbranch loc) find-key)))))
-
-;i may be searching way to hardcore with that algo
-;also it's broken at the end
 
 (defn find-node-in-branch-this-depth [loc find-key]
   (if (nil? loc) 
@@ -95,27 +57,24 @@
 ;takes an (empty-tree) as loc
 ;or an exsisting tree to add to
 (defn build-path [loc path]
-  (let [new-node {(first path) 1}]
-    (println path (zip/node loc) new-node)
-    (if (empty? path) ;this if could be higher up need to refactor
-      (zip/seq-zip (zip/root loc))
-      (let [find-key (first path)
-            nn (if (= (next-subbranch loc) nil) nil
-                   ;have to look deeper than loc's depth because
-                   ;loc is at the depth of the last added/updated node
-                   (find-node-in-branch-this-depth (next-subbranch loc) 
-                                                   find-key))]
-        (if (= nn nil)
-          (do
-            (println "INSERTING")
-            (recur
-             (zip/append-child (next-branch (add-branch loc)) new-node)
-             (rest path)))
+  (if (empty? path)
+    (zip/seq-zip (zip/root loc))
+    (let [new-node {(first path) 1}
+          find-key (first path)
+          nn (if (= (next-subbranch loc) nil) nil 
+                 (find-node-in-branch-this-depth (next-subbranch loc) 
+                                                 find-key))]
+      (if (= nn nil)
+        (do
           (recur
-           (update-node nn)
-           (rest path)))))))
+           (zip/append-child (next-branch (add-branch loc)) new-node)
+           (rest path)))
+        (recur
+         (update-node nn)
+         (rest path))))))
+
+;next the tree needs to be pruned
+;then the links between like items can be made
 
 ;some cleanup for the adam and eve dataset
 ;(def smallsample (pre-sort (map #(filter (comp not nil?) %) (take 5 (drop 3 item-titles)))))
-
-
