7054c681eadd07d9d7f2acfb4c4a7a846ca4cb0e
Frank Burnham <fra.burnham@gmail.com>
2014-12-02 21:51:31 -0500

    Working through the migration details ... slowly. Lots of interop to get right.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
             :url "http://github.com/fraburnham/fp-growth"
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
-            :local-repo "local-maven-repo"
+            :local-repo "/home/seditiosus/clojure/fp-growth/local-maven-repo"
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [Tree/Tree "0.1.0"]])
--- src/fp_growth/core.clj
+++ src/fp_growth/core.clj
@@ -4,6 +4,9 @@
 ;use the Tree.class to see if it offeres speed improvements over
 ;clojure.zip
 
+(defn alpha-by-first-char [x]
+  (sort-by (comp str first) compare x))
+
 ;;TODO: pre-sort needs to drop all items below the cutoff to speed up
 ;;TODO: tree generation. The same should be done in the clojure.zip
 ;;TODO: implementation.
@@ -12,19 +15,20 @@
 ;;placed as the first item in the list. This allows the tree building to be
 ;;simplified.
 ;;@param data A seq that is formatted ((item item item) (item item item))
+;;@return sorted data, ready for tree building
 ;;*
 (defn pre-sort [data]
-      (let [freqs (apply (partial merge-with +) (map frequencies data))]
-           (defn item-freq-list [x]
-                 (list x (x freqs)))
-           (map #(map first %)
-                (map #(sort-by last >
-                               (alpha-by-first-char (map item-freq-list %)))
-                     data))))
+  (let [freqs (apply (partial merge-with +) (map frequencies data))]
+    (defn item-freq-list [x]
+      (list x (x freqs)))
+    (map #(map first %)
+         (map #(sort-by last >
+                        (alpha-by-first-char (map item-freq-list %)))
+              data))))
 
 ;;*
 ;;An instance of the ITree interface to use for comparing and
-;;printing node values. Pass when needed as (tree-interface.)
+;;printing node values. Pass when needed as (tree-interface)
 ;;*
 (defn tree-interface []
   (reify
@@ -32,20 +36,48 @@
     (nodeDataEquals [this a b] (= a b))
     (nodeDataGreater [this a b] (> a b))
     (toString [this node]
-              (if (nil? node)
-                "Root"
-                (let [data (.-data node)]
-                     (if (nil? data)
-                       "No Data"
-                       (str data)))))))
+      (if (nil? node)
+        "Root"
+        (let [data (.-data node)]
+          (if (nil? data)
+            "No Data"
+            (str data)))))))
 
-;so the basics like node adding are already written, so let's play with some tests to build a tree
-;and see how it behaves
-(def t (new Tree))
-(.addChild (.-rootNode t) (.newNode t 1))
-(.addChild (.-rootNode t) (.newNode t 2))
-(.addChild (.-rootNode t) (.newNode t "Tree<Integer>"))
-(.addChild (.getChild (.-rootNode t) 2) (.newNode t "SUPER CHILD type means nothing to me"))
-(.printTree t (tree-interface.) (.-rootNode t))
+;reduce over the pre-sorted data?
+;check the rootNode for the first item, if it exists, inc the value
+;  if it doesn't exist add it as a child to the rootNode of the tree
+;for the next item assume that the matching node/node we just created
+;  is the new "rootNode" for the "new tree". to keep items in order
 
+;;*
+;;Check if a node has a child with the given data
+;;@param data-compare A function that returns true when both inputs match
+;;@param root-node A Tree.Node to start the search from
+;;@param child-data The value to compare against Node.data
+;;@retrun A Tree.Node if one exists nil otherwise
+;;*
+(defn get-child [data-compare root-node child-data]
+  (loop [x (.getNumChildren root-node)]
+    (if (= x -1)
+      nil ;got to the end and saw no matches along the way
+      (let [node (.getChild root-node x)] ;get the xth child of root-node
+        (if (data-compare (.-data node) child-data)
+          node ;found the node
+          (recur (dec x))))))) ;didn't find the node, dec x and check again
 
+(defn build-tree! [fn-inc-node tree items]
+  (loop [r (.-rootNode tree)
+         items items]
+    (if (empty? items)
+      tree
+      (let [find-child (get-child = r (first items))]
+        ;need to perform a check here to see if the 
+        ;child exists and update it otherwise add a new
+        ;child
+        (if (nil? find-child)
+          ;if the child doesn't exist add it
+          (.addChild r (.newNode tree (first items)))
+          ;if the child does exist update the node
+          (fn-inc-node find-child))
+        ;now recur
+        (recur (.getChild r (dec (.getNumNodes r))) (rest items))))))
