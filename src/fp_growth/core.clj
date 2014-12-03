5d140a2f33b8cd2f327ae8114df1f3fcd821444c
Frank Burnham <fra.burnham@gmail.com>
2014-12-03 10:28:39 -0500

    The tree building is ready for tests to be written. Seems faster than clojure.zip was, but that is to be expected with this much mutation.

--- src/fp_growth/core.clj
+++ src/fp_growth/core.clj
@@ -2,33 +2,37 @@
     (:import Tree ITree))
 
 ;use the Tree.class to see if it offeres speed improvements over
-;clojure.zip
+;clojure.zip. In super preliminary tests it is quicker
 
 (defn alpha-by-first-char [x]
   (sort-by (comp str first) compare x))
 
-;;TODO: pre-sort needs to drop all items below the cutoff to speed up
-;;TODO: tree generation. The same should be done in the clojure.zip
-;;TODO: implementation.
 ;;*
-;;Pre-sorts the data for fp-growth so that the most frequent item overall is
-;;placed as the first item in the list. This allows the tree building to be
-;;simplified.
-;;@param data A seq that is formatted ((item item item) (item item item))
-;;@return sorted data, ready for tree building
+; Pre-sorts the data for fp-growth so that the most frequent item overall is
+; placed as the first item in the list. This allows the tree building to be
+; simplified.
+; @param data A seq that is formatted ((item item item) (item item item))
+; @return sorted data, ready for tree building
 ;;*
-(defn pre-sort [data]
-  (let [freqs (apply (partial merge-with +) (map frequencies data))]
+(defn pre-sort [data cutoff]
+  (let [freqs (apply (partial merge-with +)
+                     (map frequencies (map #(map keyword %) data)))]
     (defn item-freq-list [x]
-      (list x (x freqs)))
-    (map #(map first %)
-         (map #(sort-by last >
-                        (alpha-by-first-char (map item-freq-list %)))
-              data))))
+      (list x ((keyword x) freqs)))
+    (remove empty?
+            (map (fn [x]
+                   (remove nil?
+                           (map first
+                                (map (fn [y]
+                                       (if (>= (last y) cutoff)
+                                         y)) x))))
+                 (map #(sort-by last >
+                                (alpha-by-first-char (map item-freq-list %)))
+                      data)))))
 
 ;;*
-;;An instance of the ITree interface to use for comparing and
-;;printing node values. Pass when needed as (tree-interface)
+; An instance of the ITree interface to use for comparing and
+; printing node values. Pass when needed as (tree-interface)
 ;;*
 (defn tree-interface []
   (reify
@@ -44,11 +48,11 @@
             (str data)))))))
 
 ;;*
-;;Check if a node has a child with the given data
-;;@param data-compare A function that returns true when both inputs match
-;;@param root-node A Tree.Node to start the search from
-;;@param child-data The value to compare against Node.data
-;;@retrun A Tree.Node if one exists nil otherwise
+; Check if a node has a child with the given data
+; @param data-compare A function that returns true when both inputs match
+; @param root-node A Tree.Node to start the search from
+; @param child-data The value to compare against Node.data
+; @retrun A Tree.Node if one exists nil otherwise
 ;;*
 (defn get-child [data-compare root-node child-data]
   (loop [x (dec (.getNumChildren root-node))]
@@ -60,22 +64,26 @@
           (recur (dec x))))))) ;didn't find the node, dec x and check again
 
 ;;*
-;;Builds the tree (in place) one transaction at a time. Update nodes
-;;if they already exist, create them otherwise. Sticks to a single path.
-;;@param fn-inc-node is the function to apply when a node is updated
-;;@param tree a Tree to build up
-;;@param items a seq in the format (itemA itemB itemC) to add to the tree
-;;@return the tree object, however the tree has already been mutated in place
+; Builds the tree (in place) one transaction at a time. Update nodes
+; if they already exist, create them otherwise. Sticks to a single path.
+; To build a tree from a list like ((item item item) (item item item)),
+; use (map (partial build-tree! ...) ...)
+; @param fn-visit-node is the function to apply when a node is updated
+; @param fn-compare-data is used to compare the current item with the child
+;        nodes' data.
+; @param tree a Tree to build up
+; @param items a seq in the format (itemA itemB itemC) to add to the tree
+; @return the tree object, however the tree has already been mutated in place
 ;;*
-(defn build-tree! [fn-inc-node tree items]
+(defn build-tree! [fn-visit-node fn-compare-data tree items]
   (loop [r (.-rootNode tree)
          items items]
     (if (empty? items)
       tree
-      (let [find-child (get-child = r (first items))]
+      (let [find-child (get-child fn-compare-data r (first items))]
         (if (nil? find-child)
           ;if the child doesn't exist add it
           (.addChild r (.newNode tree (first items)))
           ;if the child does exist update the node
-          (fn-inc-node find-child))
+          (fn-visit-node find-child))
         (recur (.getChild r (dec (.getNumChildren r))) (rest items))))))
--- test/fp_growth/core_test.clj
+++ test/fp_growth/core_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [fp-growth.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def alphanumeric "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+(defn random-string-builder [num-letters]
+  (apply str (repeatedly num-letters #(rand-nth alphanumeric))))
+
+(defn random-ticket [num-items]
+  (repeatedly num-items #(random-string-builder 3)))
+
+(defn map-visit [node]
+  (let [data (.-data node)
+        k (first (keys data))]
+    (set! (.-data node) {k (inc (k data))})))
+
+(defn map-compare [node-data data]
+  (let [nk (first (keys node-data))
+        dk (first (keys data))]
+    (= nk dk)))
+
+(defn mapify-items [items]
+  (map (fn [item]
+         {(keyword item) 1}) items))
+
+(def data (repeatedly 3000 #(random-ticket (inc (rand-int 10)))))
+
+(def sorted-mapifyd-data (map mapify-items (pre-sort data)))
+
+(map (partial build-tree! map-visit map-compare tree) sorted-mapifyd-data)
+
+;TODO: some actual testing not just REPL testing
